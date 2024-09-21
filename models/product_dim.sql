dc59fd1ed6b2fefd22e6976c2c785e5c5a4ba89c
vicky b <daceve5668@inpsur.com>
2024-09-21 15:34:31 +0000

    custom_schema add

---
+++ models/product_dim.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        materialized='table',
+        database ='dim_db',
+        schema ='dim_sch'
+
+    )
+}}
+select  
+    PRODUCTID as prod_id 
+	,NAME  as Prod_name
+	,CATEGORY as prod_category 
+	,RETAILPRICE as price
+	,RETAILPRICE * 10  as AMT
+	,UPDATED_AT  
+
+from SALES_DB.RAW.PRODUCTS
